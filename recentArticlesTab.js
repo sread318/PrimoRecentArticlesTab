c3f571fc2c741c2291cbc93ba4107d5f637c0cbc
sread318 <read.sare@gmail.com>
2016-03-01 14:08:25 -0800

    Updated “actions menu”

--- recentArticlesTab.js
+++ recentArticlesTab.js
@@ -4,6 +4,7 @@ jQuery( document ).ready(function() {
 
 	//Point to intermediary page
 	var iframeSrc = "http://library.lclark.edu/testpages/primoTab/index.php?issn=";
+<<<<<<< Updated upstream
 
 	//Change displayed name for recent articles tab
 	tabName = "Recent Articles";
@@ -22,6 +23,29 @@ jQuery( document ).ready(function() {
 	
 	var emailEnc=encodeURIComponent(email);
 
+=======
+
+	//Change displayed name for recent articles tab
+	tabName = "Recent Articles";
+
+	//Should this tab display first or last
+	firstTab = false;
+
+	//This is where the "pop-out window" will go. ISSN is added dynamically with the updateURL() function
+	url = "http://library.lclark.edu/testpages/primoTab/index.php?issn=";
+
+	//What label displays for journals/periodicals?
+	var itemType = "Journal";
+
+	//Email subscribe to journalTOC
+	var email="sararead@lclark.edu";
+
+	//If set to true, "actions" menu is displayed on the opened Recent Articles tab
+	displayActions=true;
+
+
+	var emailEnc=encodeURIComponent(email);
+>>>>>>> Stashed changes
 
 
 /**************************** New Tab *******************************/
@@ -34,6 +58,7 @@ jQuery( document ).ready(function() {
 		true);
 
 	 var evaluator = function(element){
+<<<<<<< Updated upstream
 
 			if (EXLTA_isFullDisplay() === true) {
 				var text = EXLTA_displaytype(EXLTA_recordId(element));
@@ -55,6 +80,29 @@ jQuery( document ).ready(function() {
 				}
 				else{
 
+=======
+
+			if (EXLTA_isFullDisplay() === true) {
+				var text = EXLTA_displaytype(EXLTA_recordId(element));
+
+				} else {
+				var text = $(element).parents('.EXLResult').find('.EXLThumbnailCaption').text();
+
+			}
+			if (text.toLowerCase() == itemType.toLowerCase()) {
+				var issn = EXLTA_issn(EXLTA_recordId(element));
+				var recordid=EXLTA_recordId(element);
+
+				/*  assigns unique identifier to containing div */
+				jQuery(element).parent("div").attr("id", recordid);
+
+				if (!issn){
+					//matches itemType but no issn
+					return false;
+				}
+				else{
+
+>>>>>>> Stashed changes
 					jQuery.ajax({
 					  url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.journaltocs.ac.uk%2Fapi%2Farticles%2F" + issn + "%3Fuser%3D"+ emailEnc +"&callback=?",
 					  dataType: 'jsonp',
@@ -63,15 +111,26 @@ jQuery( document ).ready(function() {
 							value = data.responseData.feed.entries[0].title;
 							jQuery.each(value, function(){
 								if(value != "0 hits" && value != "JournalTOCs API articles"){
+<<<<<<< Updated upstream
 								
 									/*  if not a false-positive, shows the tab, targeting div w/identifier */
 									$("div#"+recordid).children("ul").children("li").last().show();
 						
+=======
+
+									/*  if not a false-positive, shows the tab, targeting div w/identifier */
+									$("div#"+recordid).children("ul").children("li").last().show();
+
+>>>>>>> Stashed changes
 								}
 							});
 						}
 					});
+<<<<<<< Updated upstream
 				
+=======
+
+>>>>>>> Stashed changes
 					return true;
 
 				}  //else
@@ -83,10 +142,17 @@ jQuery( document ).ready(function() {
 
 	//Creates TOCTab
 	EXLTA_addLoadEvent(function(){
+<<<<<<< Updated upstream
 	
 		EXLTA_addTab(tabName,tabType,url,tabHandler,firstTab,evaluator);
 		updateURL(url);
 		
+=======
+
+		EXLTA_addTab(tabName,tabType,url,tabHandler,firstTab,evaluator);
+		updateURL(url);
+
+>>>>>>> Stashed changes
 	});
 
 	// Hides all the TOCTab tabs.
@@ -100,21 +166,46 @@ jQuery( document ).ready(function() {
 
 /* this dynamically updates the "popout window" button link to tack on the issn */
 function updateURL(url){
+<<<<<<< Updated upstream
 	
 	$("li.EXLContainer-recentarticlesTab").click(function(){
 		
+=======
+
+	$("li.EXLContainer-recentarticlesTab").click(function(){
+
+>>>>>>> Stashed changes
 		recordid=$(this).parent("ul").parent("div").attr("id");
 		issn= EXLTA_issn(recordid);
 		newUrl=url+issn;
 		if (EXLTA_isFullDisplay()){
 			console.log("it's full display!!");
 			$("div#"+recordid).parent("div").parent("div").parent("div").find(".EXLTabHeaderButtonPopout").children("a").attr("href", newUrl);
+<<<<<<< Updated upstream
 		}
 		else{
 			$("div#"+recordid).parent("div").parent("td").find(".EXLTabHeaderButtonPopout").children("a").attr("href", newUrl);
 		}
 	});
 
+=======
+
+			if (displayActions==true){
+				var actions=$("div#"+recordid).parent("div").parent("div").parent("div").find(".EXLTabHeaderButtons").html();
+				$(".EXLTabHeaderButtons").last().html(actions);
+			}
+		}
+		else{
+			$("div#"+recordid).parent("div").parent("td").find(".EXLTabHeaderButtonPopout").children("a").attr("href", newUrl);
+
+			if (displayActions==true){
+				var actions=$("div#"+recordid).parent("div").parent("td").find(".EXLTabHeaderButtons").html();
+				$(".EXLTabHeaderButtons").last().html(actions);
+			}
+		}
+	});
+
+>>>>>>> Stashed changes
 }
 
 
