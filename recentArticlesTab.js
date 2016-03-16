0e44fcdaef7212306374fb76552beaedb2751c9f
jeremymcwilliams <jeremym@lclark.edu>
2016-03-16 11:37:06 -0700

    initial steps to create menu function

--- recentArticlesTab.js
+++ recentArticlesTab.js
@@ -22,7 +22,9 @@ jQuery( document ).ready(function() {
 
 	//If set to true, "actions" menu is displayed on the opened Recent Articles tab
 	displayActions=true;
-
+	
+	//Your institution's VID
+	var vid="LCC";
 	
 	var emailEnc=encodeURIComponent(email);
 
@@ -127,6 +129,106 @@ function updateURL(url){
 			}
 		}
 	});
+
+}
+
+function createActionsMenu(recordid, vid){
+
+	menu="";
+	
+	menu+='<li class="EXLTabHeaderButtonSendTo">';
+  	menu+='	<a href="#" name="in'+recordid+'_51f21" id="in'+recordid+'_51f21" title="Show actions options">';
+  	menu+='	<span></span>';
+  	menu+='	Actions<img src="../images/icon_arrow_sendTo.png" alt="">';
+  	menu+='	</a>';
+	menu+='	<ol class="EXLTabHeaderButtonSendToList">';
+			
+	menu+='		<li class="EXLButtonSendToMyShelf EXLButtonSendToMyShelfAdd">';
+	menu+='							<a href="basket.do?fn=create&amp;docs='+recordid+'&amp;exemode=async" onclick="boomCallToRum("eShelf_0",false);javascript:eshelfCreate(this,"'+recordid+'","false","scope:(E-LCC),scope:(P),primo_central_multiple_fe","1");return false;" title="Add to e-Shelf" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">Add to e-Shelf</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconMyShelf"></span>';
+	menu+='							</a>';
+	menu+='						</li>';
+	menu+='						<li class="EXLButtonSendToMyShelf EXLButtonSendToMyShelfRemove" style="display: none;">';
+	menu+='							<a href="basket.do?fn=remove&amp;docs='+recordid+'&amp;exemode=async" onclick="boomCallToRum("sendTo_eshelfRemove_0",false);javascript:eshelfRemove(this,"'+recordid+'","false","scope:(E-LCC),scope:(P),primo_central_multiple_fe","1");return false;" title="Remove from e-Shelf" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">Remove from e-Shelf</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconMyShelf"></span>';
+	menu+='							</a>';
+	menu+='						</li>';
+	menu+='					<li class="EXLButtonSendToMail">';
+	menu+='						<a href="email.do?fn=email&amp;docs='+recordid+'&amp;vid=LCC&amp;fromCommand=true&amp;doc='+recordid+'&amp;scope=scope%3a(E-LCC)%2cscope%3a(P)%2cprimo_central_multiple_fe&amp;indx=1&amp;" onclick="boomCallToRum('sendTo_email_0',false);javascript:sendPrintPopOut(this);return false;" title="Send record by E-mail(opens in a new window)" target="blank">';
+	menu+='						<span class="EXLButtonSendToLabel">E-mail</span>';
+	menu+='						<span class="EXLButtonSendToIcon EXLButtonSendToIconMail"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+	menu+='				<li class="EXLButtonSendToPrint">';
+	menu+='						<a href="display.do?fn=print&amp;tab=default_tab&amp;indx=1&amp;display=print&amp;docs='+recordid+'&amp;indx=1&amp;" onclick="boomCallToRum('sendTo_print_0',false);javascript:sendPrintPopOut(this);return false;" title="Print record (opens in a new window)" target="blank">';
+	menu+='						<span class="EXLButtonSendToLabel">Print</span>';
+	menu+='						<span class="EXLButtonSendToIcon EXLButtonSendToIconPrint"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+	menu+='				<li class="EXLButtonSendToPermalink">';
+	menu+='				  			<a href="permalink.do?docId='+recordid+'&amp;vid=LCC&amp;fn=permalink" onclick="boomCallToRum('sendTo_Permalink_0',false);javascript:openPermaLinkLbox('permalink','docId='+recordid+'&amp;vid=LCC&amp;fn=permalink','0',''+recordid+'');return false;" title="Permanent URL for this record" target="blank">';
+	menu+='				  			<span class="EXLButtonSendToLabel">Permalink</span>';
+	menu+='				  			<span class="EXLButtonSendToIcon EXLButtonSendToIconPermalink"></span></a>';
+	menu+='				  		</li>';
+	menu+='			  		<li class="EXLButtonSendToCitation">';
+	menu+='						<a href="#" onclick="boomCallToRum('sendTo_citation_0',false);openCitationLbox('0',''+recordid+'');return false;" title="Bibliographic citation for this title" target="blank">';
+	menu+='						<span class="EXLButtonSendToLabel">Citation</span> ';
+	menu+='						<span class="EXLButtonSendToIcon EXLButtonSendToIconCitation"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+	menu+='				<li class="EXLButtonSendToEasyBib">';
+	menu+='						<a href="PushToAction.do?recId='+recordid+'&amp;pushToType=EasyBib&amp;fromEshelf=false" title="Add toEasyBib" onclick="boomCallToRum('sendTo_pushto_0_5',false);pushto('EasyBib','1','false',''+recordid+'');return false;" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">';
+	menu+='								EasyBib</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconEasyBib"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+						
+	menu+='				<li class="EXLButtonSendToEndNote">';
+	menu+='						<a href="PushToAction.do?recId='+recordid+'&amp;pushToType=EndNote&amp;fromEshelf=false" title="Add toEndNote" onclick="boomCallToRum('sendTo_pushto_0_6',false);pushto('EndNote','1','false',''+recordid+'');return false;" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">';
+	menu+='								EndNote</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconEndNote"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+						
+	menu+='				<li class="EXLButtonSendToRefWorks">';
+	menu+='						<a href="PushToAction.do?recId='+recordid+'&amp;pushToType=RefWorks&amp;fromEshelf=false" title="Add toRefWorks" onclick="boomCallToRum('sendTo_pushto_0_7',false);pushto('RefWorks','1','false',''+recordid+'');return false;" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">';
+	menu+='								RefWorks</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconRefWorks"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+						
+	menu+='				<li class="EXLButtonSendToRISPushTo">';
+	menu+='						<a href="PushToAction.do?recId='+recordid+'&amp;pushToType=RISPushTo&amp;fromEshelf=false" title="Add toRISPushTo" onclick="boomCallToRum('sendTo_pushto_0_8',false);pushto('RISPushTo','1','false',''+recordid+'');return false;" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">';
+	menu+='								Export RIS</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconRISPushTo"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+						
+	menu+='				<li class="EXLButtonSendToDelicious">';
+	menu+='						<a href="PushToAction.do?recId='+recordid+'&amp;pushToType=Delicious&amp;fromEshelf=false" title="Add toDelicious" onclick="boomCallToRum('sendTo_pushto_0_10',false);pushto('Delicious','1','false',''+recordid+'');return false;" target="blank">';
+	menu+='							<span class="EXLButtonSendToLabel">';
+	menu+='								Delicious</span>';
+	menu+='							<span class="EXLButtonSendToIcon EXLButtonSendToIconDelicious"></span>';
+	menu+='						</a>';
+	menu+='					</li>';
+						
+	menu+='				</ol>';
+	menu+='</li>';
+
+
+
+
+
+
+
+
+
 
 }
 
